94d6d905d64276e7fd1f0766958299fbc0b815bd
Xitee <59659167+Xitee1@users.noreply.github.com>
2024-09-14 11:57:54 +0200

    Remove phantom spawn prevention because there's now a gamerule for it (doInsomnia)

--- src/main/java/de/xite/smp/listener/entity/EntitySpawnListener.java
+++ src/main/java/de/xite/smp/listener/entity/EntitySpawnListener.java
@@ -1,16 +1,7 @@
 package de.xite.smp.listener.entity;
 
-import org.bukkit.entity.EntityType;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntitySpawnEvent;
-import org.jetbrains.annotations.NotNull;
 
-public class EntitySpawnListener implements @NotNull Listener {
-	@EventHandler
-	public void onSpawn(EntitySpawnEvent e) {
-		if(e.getEntityType() == EntityType.PHANTOM) {
-			e.setCancelled(true);
-		}
-	}
+public class EntitySpawnListener implements Listener {
+
 }
